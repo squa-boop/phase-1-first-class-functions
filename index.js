145a053ff4a9cd5fdca690e537930dc571bb4ce2
blessed wesonga <blessedwesonga@gmail.com>
2024-10-02 20:00:15 +0300

    phase-1-first-class-functions

---
+++ index.js
@@ -0,0 +1,17 @@
+const { spy } = require("chai");
+
+function receivesAFunction(callback) {
+    callback(); 
+}
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log("This is a named function!");
+    }
+    return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("This is an anonymous function!");
+    };
+}
